353d7119845132efcde268904a629220991374f6
Marcio Araya <marcio.araya@df.senac.br>
2022-05-20 11:12:58 -0300

    acrescenta comentários

--- aula09-arquivos-texto/exemplos/exemplo-fwrite.php
+++ aula09-arquivos-texto/exemplos/exemplo-fwrite.php
@@ -1,7 +1,9 @@
 <?php
-
+# Quebra de linha no Windows é definida
+# pelos caracteres CR e LF
+# chr(13) - CR
+# chr(10) - LF
 define("EOL", chr(13).chr(10));
-
 
 # fonte:
 # https://www.php.net/manual/pt_BR/function.fwrite.php
@@ -10,7 +12,7 @@ echo '<h1>estou funcionando</h1>';
 $pessoa["nome"] = "Marcio";
 $pessoa["dt_nasc"] = "1968-01-01";
 $pessoa["sexo"] = "M";
-
+# EOL = End Of Line : Fim de Linha (quebra de linha)
 $linha = json_encode($pessoa) . EOL;
 
 echo $linha."<br>";
@@ -18,5 +20,5 @@ $fp = fopen('data.txt', 'w');
 fwrite($fp, $linha);
 fclose($fp);
 echo '<h1>Acabei</h1>';
-// o conteúdo de 'data.txt' agora é 123 e não 23!
+
 ?>
