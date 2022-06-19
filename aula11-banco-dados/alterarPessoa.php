3c17068d96d7f52eb66d110ebaf0f0f4d0381af9
Marcio Araya <marcio.araya@gmail.com>
2022-06-19 16:51:09 -0300

    alteração de pessoa

--- aula11-banco-dados/alterarPessoa.php
+++ aula11-banco-dados/alterarPessoa.php
@@ -1,5 +1,7 @@
 <?php
-include "inc/conectabd.php"
+include "inc/conectabd.php";
+include "inc/funcoes.php";
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -14,34 +16,42 @@ include "inc/conectabd.php"
     <?php
 
     if (!isset($_GET["bt_sub"])) {
-      // primeira vez, então exibe formulário
+        // primeira vez, então exibe formulário
+
+        # recupera o id passado pela página principal
+        $id_pessoa = $_GET["id"];
+        # usa a função lerPessoa() para recuperar os demais dados correspondentes ao id_pessoa recebido
+        # $pessoa é um vetor cujos índices são os nomes das colunas da tabela tb_pessoa
+        $pessoa = lerPessoa($pdo, $id_pessoa);
     ?>
       <form  action="" method="get">
+        <input type="hidden" name="id_pessoa" value="<?php echo $id_pessoa ?>">
         <label for="nome">Nome</label>
-        <input type="text" name="ds_nome" id="nome">
+        <input type="text" name="ds_nome" id="nome" value="<?php echo $pessoa["ds_nome"]; ?>" >
         <br>
-        <input type="radio" name="cd_sexo" id="masc" value="M">
+        <input type="radio" name="cd_sexo" id="masc" value="M" <?php echo $pessoa["cd_sexo"]=="M"?"checked":"" ?> >
         <label for="masc">Masculino</label>
-        <input type="radio" name="cd_sexo" id="fem" value="F">
+        <input type="radio" name="cd_sexo" id="fem" value="F" <?php echo $pessoa["cd_sexo"]=="F"?"checked":"" ?> >
         <label for="fem">Feminino</label>
-        <input type="radio" name="cd_sexo" id="ndef" value="N">
+        <input type="radio" name="cd_sexo" id="ndef" value="N" <?php echo $pessoa["cd_sexo"]=="N"?"checked":"" ?> >
         <label for="ndef">Prefiro não informar</label>
         <br>
         <label for="dtnasc">Data Nascimento</label>
-        <input type="date" name="dt_nasc" id="dtnasc">
+        <input type="date" name="dt_nasc" id="dtnasc" value="<?php echo $pessoa["dt_nasc"] ?>">
         <br>
         <label for="email">E-mail</label>
-        <input type="text" name="ds_email" id="email">
+        <input type="text" name="ds_email" id="email" value="<?php echo $pessoa["ds_email"] ?>">
         <br>
         <label for="telefone">Telefone</label>
-        <input type="text" name="nr_telefone" id="telefone">
+        <input type="text" name="nr_telefone" id="telefone"  value="<?php echo $pessoa["nr_telefone"] ?>">
         <br>
-        <input type="submit" name="bt_sub" value="Inserir">
+        <input type="submit" name="bt_sub" value="Alterar">
       </form>
 
     <?php
 
     } else {
+      $id_pessoa = $_GET["id_pessoa"];
       $ds_nome = $_GET["ds_nome"];
       $cd_sexo = $_GET["cd_sexo"];
       $dt_nasc = $_GET["dt_nasc"]; # ano - mes - dia
@@ -49,7 +59,8 @@ include "inc/conectabd.php"
       $ds_email = $_GET["ds_email"];
   
       try {
-          $sql = 'INSERT INTO `agenda`.`tb_pessoa` (`ds_nome`,`cd_sexo`,`dt_nasc`,`nr_telefone`,`ds_email`)  VALUES (:ds_nome,:cd_sexo,:dt_nasc,:nr_telefone,:ds_email);';
+        
+          $sql = 'UPDATE `agenda`.`tb_pessoa` SET `ds_nome`=:ds_nome, `cd_sexo`=:cd_sexo, `dt_nasc`=:dt_nasc, `nr_telefone`=:nr_telefone, `ds_email`=:ds_email WHERE `id_pessoa` = :id_pessoa  ;';
           //echo ">>>sql ".$sql. "  fim <<<<";
           $stmt = $pdo->prepare($sql);
           $stmt->bindParam(':ds_nome', $ds_nome);
@@ -58,6 +69,7 @@ include "inc/conectabd.php"
           $stmt->bindParam(':dt_nasc', $dt_nasc);
           $stmt->bindParam(':nr_telefone', $nr_telefone);
           $stmt->bindParam(':ds_email', $ds_email);
+          $stmt->bindParam(':id_pessoa', $id_pessoa);
           $stmt->execute();
         
           if ($stmt->rowCount()>0) {
--- aula11-banco-dados/inc/funcoes.php
+++ aula11-banco-dados/inc/funcoes.php
@@ -7,6 +7,9 @@ function lerPessoa($pdo, $id_pessoa){
     $statement = $pdo->prepare($sql);
     $statement->bindParam(':id_pessoa', $id_pessoa);
     $statement->execute();
+    # PDO::FETCH_ASSOC - os dados lidos são retornados em um vetor associativo, isto é, 
+    # os índices do vetor não são numéricos, são strings, e neste caso
+    # serão os nomes das colunas indicados no SELECT
     return $statement->fetch(PDO::FETCH_ASSOC);
 
 }
