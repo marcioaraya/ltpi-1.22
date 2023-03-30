1a63a1196c9b2a0578166fe95cd4c8832a49ca67
Marcio Araya <marcio.araya@gmail.com>
2023-03-30 09:54:50 -0300

    atualiza mensagem

--- aula09-arquivos-texto/exemplos/exemplo-fgets4.php
+++ aula09-arquivos-texto/exemplos/exemplo-fgets4.php
@@ -1,12 +1,12 @@
 <?php
-
 # fonte: https://www.php.net/manual/pt_BR/function.fgets.php
-$handle = fopen("data.txt", "r");
+$handle = fopen("notas.txt", "r");
 if ($handle) {
     echo "<table border=\"1\">";
-    echo "<tr><th>Nome</th><th>Dt Nasc</th><th>Sexo</th></tr>";
+    echo "<tr><th>Nome</th><th>Média</th><th>Sexo</th></tr>";
     while (($buffer = fgets($handle, 4096)) !== false) {
-        $pessoa = json_decode($buffer);
+        $pessoa = json_decode($buffer, true);
+        print_r($pessoa);
         echo "<tr>";
         foreach($pessoa as $p=>$v){
             echo "<td>$v</td>";
